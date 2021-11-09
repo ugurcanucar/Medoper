d48008e79bccaf4fac86303b4d3707c6b8ce6ae1
Uğurcan Uçar <ugurcanucar07@hotmail.com>
2021-11-09 18:08:14 +0300

    uu
    up

--- src/views/app-views/Home/index.js
+++ src/views/app-views/Home/index.js
@@ -1,13 +1,90 @@
-import Loading from 'components/Shared-Components/Loading'
-import React from 'react'
+import Loading from "components/Shared-Components/Loading";
+import React from "react";
 
 const Home = () => {
-    return (
-        <div className="relative">
+  document.addEventListener("scroll", (event) => {
+    const top = document.body.getBoundingClientRect().top;
+    const header = document.getElementById("header");
 
-            <p>Content burada olacak!!!!</p>
-        </div>
-    )
-}
+    if (header.classList.contains("bg-red-500") === true) {
+      header.classList.remove("bg-red-500", "h-12");
+    }
 
-export default Home
+    if (header.classList.contains("bg-red-500") === false && top < -200) {
+      header.classList.add("bg-red-500 ", "h-12");
+      return;
+    }
+  });
+  return (
+    <div className="min-w-screen h-full bg-blue-400">
+      <div
+        id="header"
+        className="bg-black top-0 w-screen h-52 sticky transition duration-1000"
+      ></div>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+      <p>aawww</p>
+    </div>
+  );
+};
+
+export default Home;
